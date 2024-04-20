634acdcd24f32bc441928bd104d70d460df02183
unknown <svicovkirill@gmail.com>
2024-04-20 21:36:57 +0300

    Added admins heandlers

--- app.py
+++ app.py
@@ -2,19 +2,23 @@ import asyncio
 import os
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.fsm.strategy import FSMStrategy
 from dotenv import load_dotenv, find_dotenv
 
 from common.bot_command_list import private
 from handlers.user_group import user_group_router
 from handlers.user_private import user_private_router
+from handlers.admin_private import admin_router
 
 load_dotenv(find_dotenv())
 
 ALLOWED_UPDATES = ['message', 'edited_message']
 
 bot = Bot(token=os.getenv('TOKEN'))
+bot.my_admins_list = []
 dp = Dispatcher()
-dp.include_routers(user_private_router, user_group_router)
+dp.include_routers(user_private_router, user_group_router, admin_router)
 
 
 async def main():
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,8 +1,9 @@
 from aiogram import types, Router, F
 from aiogram.filters import CommandStart, Command, or_f
+from aiogram.utils.formatting import as_list, as_marked_section, Bold
 
 from filters.chat_types import ChatTypeFilter
-from keyboards.reply import start_kb, start_kb2, start_kb3, del_kbd, test_keyboard
+from keyboards.reply import get_keyboard
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(['private', ]))
@@ -10,17 +11,31 @@ user_private_router.message.filter(ChatTypeFilter(['private', ]))
 
 @user_private_router.message(CommandStart())
 async def start_cmd(message: types.Message):
-    await message.answer('Привет! Я виртуальный помощник.',
-                         reply_markup=start_kb3.as_markup(
-                             resize_keybord=True,
-                             input_field_placeholder='Что вас интересует?',
-                         ))
+    await message.answer(
+        'Привет! Я виртуальный помощник.',
+        reply_markup=get_keyboard(
+            'Меню',
+            'О магазине',
+            'Варианты оплаты',
+            'Варианты доставки',
+            placeholder='Что вас интересует?',
+            sizes=(2, 2),
+        )
+    )
 
 
 # @user_private_router.message(F.text.lower().contains('меню'))
-@user_private_router.message(or_f(Command('menu'), F.text.lower() == 'меню'))
+@user_private_router.message(or_f(Command('menu'), F.text.lower() == 'меню', CommandStart()))
 async def menu_cmd(message: types.Message):
-    await message.answer('Вот меню:', reply_markup=test_keyboard)
+    await message.answer('Привет! Я виртуальный помощник.',
+                         reply_markup=get_keyboard(
+                             'Меню',
+                             'О магазине',
+                             'Варианты оплаты',
+                             'Варианты доставки',
+                             placeholder='Что вас интересует?',
+                             sizes=(2, 2),
+                         ))
 
 
 @user_private_router.message(F.text.lower().contains('о нас'))
@@ -32,13 +47,36 @@ async def about_cmd(message: types.Message):
 @user_private_router.message(F.text.lower().contains('оплат'))
 @user_private_router.message(Command('payment'))
 async def payment_cmd(message: types.Message):
-    await message.answer('Способы оплаты:')
+    text = as_marked_section(
+        Bold('Варианты оплаты:'),
+        'Картой в боте',
+        'При получении карта/наличные',
+        'В заведении',
+        marker='✔ '
+    )
+    await message.answer(text.as_html())
 
 
 @user_private_router.message(F.text.lower().contains('доставк'))
 @user_private_router.message(Command('shipping'))
 async def shipping_cmd(message: types.Message):
-    await message.answer('Способы доставки:')
+    text = as_list(
+        as_marked_section(
+            Bold('Варианты доставки:'),
+            'Курьер',
+            'Самовывоз',
+            'В заведении',
+            marker='✔ '
+        ),
+        as_marked_section(
+            Bold('Невозможно: '),
+            'Почта',
+            'Голуби',
+            marker='❌ '
+        ),
+        sep='\n---------------------------------\n',
+    )
+    await message.answer(text.as_html())
 
 
 @user_private_router.message(F.contact)
