710d71ee697dcd409f581452bfe0f9480ab0834d
unknown <svicovkirill@gmail.com>
2024-04-22 17:45:31 +0300

    Final commit

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from middleweares.db import DataBaseSession
 load_dotenv(find_dotenv())
 
 from common.bot_command_list import private
-from database.engine import create_db, drop_db, session_marker
+from database.engine import create_db, drop_db, session_maker
 from handlers.user_group import user_group_router
 from handlers.user_private import user_private_router
 from handlers.admin_private import admin_router
@@ -20,7 +20,7 @@ from handlers.admin_private import admin_router
 # from middleweares.db import CounterMiddleware
 
 
-ALLOWED_UPDATES = ['message', 'edited_message']
+ALLOWED_UPDATES = ['message', 'edited_message', 'callback_query']
 
 bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 bot.my_admins_list = []
@@ -29,9 +29,7 @@ dp.include_routers(user_private_router, user_group_router, admin_router)
 
 
 async def on_startup(bot):
-    run_param = False
-    if run_param:
-        await drop_db()
+   # await drop_db() # Закоментить после 1го запуска
     await create_db()
 
 
@@ -42,12 +40,12 @@ async def on_shutdown(bot):
 async def main():
     dp.startup.register(on_startup)
     dp.shutdown.register(on_shutdown)
-    dp.update.middleware(DataBaseSession(session_pool=session_marker))
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
     await create_db()
     await bot.delete_webhook(drop_pending_updates=True)
-    await bot.set_my_commands(commands=private,
-                              scope=types.BotCommandScopeAllPrivateChats())
-    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
+    #await bot.set_my_commands(commands=private,
+                              #scope=types.BotCommandScopeAllPrivateChats())
+    #await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats()) # Закоментить после первого запуска
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
 
 
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,91 +1,55 @@
-from aiogram import types, Router, F
-from aiogram.filters import CommandStart, Command, or_f
-from aiogram.utils.formatting import as_list, as_marked_section, Bold
+from aiogram import F, types, Router
+from aiogram.filters import CommandStart
+
+from sqlalchemy.ext.asyncio import AsyncSession
+from database.orm_query import (
+    orm_add_to_cart,
+    orm_add_user,
+)
 
 from filters.chat_types import ChatTypeFilter
-from keyboards.reply import get_keyboard
+from handlers.menu_processing import get_menu_content
+from keyboards.inline import MenuCallBack, get_callback_btns
 
 user_private_router = Router()
-user_private_router.message.filter(ChatTypeFilter(['private', ]))
+user_private_router.message.filter(ChatTypeFilter(["private"]))
 
 
 @user_private_router.message(CommandStart())
-async def start_cmd(message: types.Message):
-    await message.answer(
-        'Привет! Я виртуальный помощник.',
-        reply_markup=get_keyboard(
-            'Меню',
-            'О магазине',
-            'Варианты оплаты',
-            'Варианты доставки',
-            placeholder='Что вас интересует?',
-            sizes=(2, 2),
-        )
+async def start_cmd(message: types.Message, session: AsyncSession):
+    media, reply_markup = await get_menu_content(session, level=0, menu_name="main")
+
+    await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
+
+
+async def add_to_cart(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    user = callback.from_user
+    await orm_add_user(
+        session,
+        user_id=user.id,
+        first_name=user.first_name,
+        last_name=user.last_name,
+        phone=None,
+    )
+    await orm_add_to_cart(session, user_id=user.id, product_id=callback_data.product_id)
+    await callback.answer("Товар добавлен в корзину.")
+
+
+@user_private_router.callback_query(MenuCallBack.filter())
+async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    if callback_data.menu_name == "add_to_cart":
+        await add_to_cart(callback, callback_data, session)
+        return
+
+    media, reply_markup = await get_menu_content(
+        session,
+        level=callback_data.level,
+        menu_name=callback_data.menu_name,
+        category=callback_data.category,
+        page=callback_data.page,
+        product_id=callback_data.product_id,
+        user_id=callback.from_user.id,
     )
 
-
-# @user_private_router.message(F.text.lower().contains('меню'))
-@user_private_router.message(or_f(Command('menu'), F.text.lower() == 'меню', CommandStart()))
-async def menu_cmd(message: types.Message):
-    await message.answer('Привет! Я виртуальный помощник.',
-                         reply_markup=get_keyboard(
-                             'Меню',
-                             'О магазине',
-                             'Варианты оплаты',
-                             'Варианты доставки',
-                             placeholder='Что вас интересует?',
-                             sizes=(2, 2),
-                         ))
-
-
-@user_private_router.message(F.text.lower().contains('о нас'))
-@user_private_router.message(Command('about'))
-async def about_cmd(message: types.Message):
-    await message.answer('О нас:')
-
-
-@user_private_router.message(F.text.lower().contains('оплат'))
-@user_private_router.message(Command('payment'))
-async def payment_cmd(message: types.Message):
-    text = as_marked_section(
-        Bold('Варианты оплаты:'),
-        'Картой в боте',
-        'При получении карта/наличные',
-        'В заведении',
-        marker='✔ '
-    )
-    await message.answer(text.as_html())
-
-
-@user_private_router.message(F.text.lower().contains('доставк'))
-@user_private_router.message(Command('shipping'))
-async def shipping_cmd(message: types.Message):
-    text = as_list(
-        as_marked_section(
-            Bold('Варианты доставки:'),
-            'Курьер',
-            'Самовывоз',
-            'В заведении',
-            marker='✔ '
-        ),
-        as_marked_section(
-            Bold('Невозможно: '),
-            'Почта',
-            'Голуби',
-            marker='❌ '
-        ),
-        sep='\n---------------------------------\n',
-    )
-    await message.answer(text.as_html())
-
-
-@user_private_router.message(F.contact)
-async def get_contact(message: types.Message):
-    await message.answer(f'Номер получен')
-    await message.answer(str(message.contact))
-
-
-@user_private_router.message(F.location)
-async def get_location(message: types.Message):
-    await message.answer(f'Локация получен')
-    await message.answer(str(message.location))
+    await callback.message.edit_media(media=media, reply_markup=reply_markup)
+    await callback.answer()
